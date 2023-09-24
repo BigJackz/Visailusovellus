0722605e5d6d3f25ade94362a5e49c77655c4db5
Joona <joonajarasanen@gmail.com>
2023-09-24 20:25:37 +0300

    schema.sql added

---
+++ schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE questions (id SERIAL PRIMARY KEY, question TEXT);
+
+CREATE TABLE answers (id SERIAL PRIMARY KEY, question_id INTEGER REFERENCES questions, answer1 TEXT, answer2 TEXT, answer3, TEXT, answer4 TEXT);
+
+CREATE TABLE correct (id SERIAL PRIMARY KEY, question_id INTEGER REFERENCES questions, answer TEXT);
+
+
