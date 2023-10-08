875936d5bf588e0dc8e0ea58fcee7264f4d1fc37
Joona <joonajarasanen@gmail.com>
2023-10-08 22:54:15 +0300

    tools.py modified

--- tools.py
+++ tools.py
@@ -3,3 +3,9 @@
 def make_string(input):
     input = str(input).strip('(' + ')' + ',' + "'" + '"')
     return input
+
+#return False if string is not inbetween min and max, otherwise returns True
+def check_length(string, min, max):
+    if len(string) > max or len(string) < min:
+        return False
+    return True
