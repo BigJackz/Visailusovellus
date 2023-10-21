0b5a15f99b2cb4cddac76d15bbbc0e70bbf6b82f
Joona <joonajarasanen@gmail.com>
2023-10-21 18:34:58 +0300

    schema.sql added 1 new table and udpated questions table

--- schema.sql
+++ schema.sql
@@ -1,4 +1,6 @@
-CREATE TABLE questions (id SERIAL PRIMARY KEY, question TEXT);
+CREATE TABLE topics (id SERIAL PRIMARY KEY, topic TEXT UNIQUE);
+
+CREATE TABLE questions (id SERIAL PRIMARY KEY, topic_id INTEGER REFERENCES topics, question TEXT);
 
 CREATE TABLE answers (id SERIAL PRIMARY KEY, question_id INTEGER REFERENCES questions, answer1 TEXT, answer2 TEXT, answer3 TEXT, answer4 TEXT);
 
