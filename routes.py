076ce6a80c87ee0a1048d196e23787a689ad695c
Joona <joonajarasanen@gmail.com>
2023-10-21 20:05:05 +0300

    routes.py added minimal login and logout routes

--- routes.py
+++ routes.py
@@ -1,10 +1,26 @@
 from app import app
-from flask import redirect, render_template
-from questions import get_all_questions, get_question_and_answers, get_result, send_question
+from flask import redirect, render_template, session
+from questions import get_all_questions, get_question_and_answers, get_result, send_question, loginn
+from os import getenv
+
+app.secret_key = getenv("SECRET_KEY")
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+
+@app.route("/login", methods=["POST"])
+def login():
+    username = loginn()
+    session["username"] = username
+    return redirect("/")
+
+@app.route("/logout")
+def logout():
+    del session["username"]
+    return redirect("/")
+
 
 @app.route("/new")
 def new():
@@ -16,7 +32,10 @@ def send():
     if answer == 1:
         return redirect("/success")
     elif answer == 2:
-        return render_template("error.html", error = "Question or one of the answers is too long or short! Question must be between 1 and 100 characters and answers between 1 and 30 characters.")
+        #quite fat error message maybe make 3 separate errors at some point
+        return (render_template("error.html", error = "Question, topic or one of the answers i"
+        "s too long or short! Question must be between 1 and 100 characters, topic between 1 and 20 characters"
+        " and answers between 1 and 30 characters."))
     elif answer == 3:
         return render_template("error.html", error = "Right answer can't be the same as a wrong answer!")
     elif answer == 4:
@@ -29,7 +48,9 @@ def success():
 @app.route("/get_question", methods=["GET"])
 def get_question():
     random_order, question, final_answers,id = get_question_and_answers(-1)
-    return render_template("answer.html", question=question, answer1=final_answers[random_order[0]], answer2=final_answers[random_order[1]], answer3=final_answers[random_order[2]], answer4=final_answers[random_order[3]], id = id)
+    return (render_template("answer.html", question=question, answer1=final_answers[random_order[0]],
+            answer2=final_answers[random_order[1]], answer3=final_answers[random_order[2]],
+            answer4=final_answers[random_order[3]], id = id))
 
 @app.route("/answer")
 def answer():
@@ -53,4 +74,6 @@ def get_this_question():
     #id = int(request.form["id"])
     #print(id)
     random_order, question, final_answers, id = get_question_and_answers(-1)
-    return render_template("answer.html", question=question, answer1=final_answers[random_order[0]], answer2=final_answers[random_order[1]], answer3=final_answers[random_order[2]], answer4=final_answers[random_order[3]], id = id)
+    return (render_template("answer.html", question=question, answer1=final_answers[random_order[0]],
+            answer2=final_answers[random_order[1]], answer3=final_answers[random_order[2]],
+            answer4=final_answers[random_order[3]], id = id))
